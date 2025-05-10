7feb9ef6b20a097017ece8a761c681ece9f3ef47
Swayam Patil <patilswayam96@gmail.com>
2025-05-10 18:25:43 +0530

    routes fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { getAccessToken } from "./utils/tokenStorage"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000'
+    baseURL: '' || 'http://localhost:8000'
 })
 
 api.interceptors.request.use(
